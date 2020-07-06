eaa06286947691b46042982c99dfc727f3c5e3b9
irakli-ika <irakliqiria8@gmail.com>
2020-07-07 02:19:37 +0400

    add reset button... some errors

--- myJs/script/dom/input/index.js
+++ myJs/script/dom/input/index.js
@@ -1,10 +1,18 @@
-const txtContainer = document.querySelector('.txtContainer')
+let txtContainer = document.querySelector('.txtContainer')
+const opacity = document.getElementById('opacity')
+const size = document.getElementById('size')
+const bold = document.getElementById('bold')
+const italic = document.getElementById('italic')
+const underline = document.getElementById('underline')
 const opacityOutput = document.getElementById('opacityOutput')
 const sizeOutput = document.getElementById('sizeOutput')
 const enterText = document.querySelector('.enterTxt')
 const enterLabel = document.querySelector('.enterLabel')
 const clearLabel = document.querySelector('.clearLabel')
 const resetLabel = document.querySelector('.resetLabel')
+
+
+
 
 const onChangeTxt = (e) => {
     const text = e.target.value
@@ -63,3 +71,17 @@ const onColorBlue = (e) => {
     const colorBlue = e.target.checked
     if (colorBlue) txtContainer.style.color = 'Blue'
 }
+
+const eventResetLabel = resetLabel.addEventListener('click', (e) => {
+    opacity.value = 1
+    opacityOutput.value = 1
+
+    size.value = 16
+    sizeOutput.value = `16px`
+
+    bold.checked = false
+    italic.checked = false
+    underline.checked = false
+
+    txtContainer = txtContainer.style.opacity = 1
+})
