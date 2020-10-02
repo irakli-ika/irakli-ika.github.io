c6ebc75b52a522091f707da19f981ec074a2c8da
irakli-ika <irakliqiria8@gmail.com>
2020-10-02 19:22:10 +0400

    corrected filter, add modal window

--- myJs/script/dom/movie_project/js/filterFunction.js
+++ myJs/script/dom/movie_project/js/filterFunction.js
@@ -8,31 +8,50 @@ function filterStructure(){
             return movies.filter((item) => {
                 if(item.name.includes(input)) {
 
-                        filteredCols.push(`<div class="main_col col-md-3 bg-secondary m-2 pt-2 rounded-lg">
-                                                <div class="content text-center">
-                                                    <a href="page.html#${item.id}" class="text-decoration-none">
-                                                        <div class="movie_name">
-                                                            <h5 class="item">${item.name}</h5>
-                                                        </div>
-                                                        <div class="movie_img">
-                                                            <img src="img/${item.image}" alt="">
-                                                        </div>
-                                                        <div class="more text-right">
-                                                            <p>სრულად</p>
-                                                        </div>
-                                                    </a>
-                                                </div>
-                                            </div>`)
-        
+                    filteredCols.push(`<div class="main_col col-md-3 bg-secondary m-2 pt-2 rounded-lg">
+                                            <div class="content text-center">
+                                                <a href="page.html#${item.id}" class="text-decoration-none">
+                                                    <div class="movie_name">
+                                                        <h5 class="item">${item.name}</h5>
+                                                    </div>
+                                                    <div class="movie_img">
+                                                        <img src="img/${item.image}" alt="">
+                                                    </div>
+                                                    <div class="more text-right">
+                                                        <p>სრულად</p>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </div>`)
                 }
             })
-
         }
-         
+
         filteredMovies()
-    
-        document.getElementById('main_row').innerHTML = filteredCols.map(element => element).join('')
- 
+
+        const filterPromise = []
+        if (input.trim() === '' ) {
+            
+            movies.slice(0, 3).forEach((e) => {
+                filterPromise.push(`<div class="main_col col-md-3 bg-secondary m-2 pt-2 rounded-lg">
+                                    <div class="content text-center">
+                                        <a href="page.html#${e.id}" class="text-decoration-none">
+                                            <div class="movie_name">
+                                                <h5 class="item">${e.name}</h5>
+                                            </div>
+                                            <div class="movie_img">
+                                                <img src="img/${e.image}" alt="">
+                                            </div>
+                                            <div class="more text-right">
+                                                <p>სრულად</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>`)})
+
+            document.getElementById('main_row').innerHTML = filterPromise.map((e) => e).join('')
+        }
+        else document.getElementById('main_row').innerHTML = filteredCols.map(element => element).join('')
     })
 }
 //? end filter function
--- myJs/script/dom/movie_project/js/tempFunction.js
+++ myJs/script/dom/movie_project/js/tempFunction.js
@@ -1,33 +1,13 @@
-// function pushInCols() {
-
-//     return movies.forEach(element => {
-
-//         cols.push(`<div class="main_col col-md-3 bg-secondary m-2 pt-2 rounded-lg">
-//                         <div class="content text-center">
-//                             <a href="page.html#${element.id}" class="text-decoration-none">
-//                                 <div class="movi_name">
-//                                     <h5 class="item">${element.name}</h5>
-//                                 </div>
-//                                 <div class="movie_img">
-//                                     <img src="img/${element.image}" alt="">
-//                                 </div>
-//                                 <div class="more text-right">
-//                                     <p>სრულად</p>
-//                                 </div>
-//                             </a>
-//                         </div>
-//                     </div>`)
-//     })
-
-// }
-//? end pushInCols function
-
-function pushInRow(){
+function pushInModal(){
 
     return movies.forEach(element => {
 
-        row.push(`<div class="row mx-auto page_row view" id="${element.id}">
-                        <div class="col-md-9 mx-auto bg-secondary p-3 rounded-lg">
+        modal.push(`<div class="row myModal mx-auto page_row view pt-5" id="${element.id}">
+        
+                        <div class="col-md-9 mx-auto bg-secondary p-3 rounded-lg" style= "height: 395px" >
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true" class="closeModal" style= "font-size: 30px">&times;</span>
+                            </button>    
                             <div class="row col_row">
                                 <div class="col-md-4 img">
                                     <img src="img/${element.image}" alt="">
